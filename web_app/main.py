cf2849fb4e13b257eaa618d9358dabbfa5fabb06
Prakya-tech105 <prakyakuncham@gmail.com>
2026-04-07 09:20:12 +0530

    small change

--- web_app/main.py
+++ web_app/main.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('main.html',titke="Home Page")
+    return render_template('index.html',titke="Home Page")
 
 if __name__=='__main__':
     #Local Development server
